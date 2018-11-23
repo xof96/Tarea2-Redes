7c195246c85f3abc13b7d09c9aa48030f3e9db7e
Matías Pizarro <xof96@github.com>
2018-11-23 19:56:09 -0300

    everything is new

---
+++ go_back_n/receiver.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+from utils.conn import receiver_handshake_conn
+
+BUF = 1024
+
+if __name__ == '__main__':
+
+    # Verificamos que vengan los parámetros.
+    if len(sys.argv) != 2:
+        print("python receiver.py [PORT_NUMBER]")
+
+    SW_IP = ""
+    SW_PORT = int(sys.argv[1])
+
+    # Armamos el socket.
+    the_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Asociamos el socket a la dirección y el puerto especificados.
+    the_socket.bind((SW_IP, SW_PORT))
+
+    # Establecemos los parámetros.
+
+    if not receiver_handshake_conn(the_socket, BUF, 3):
+        raise Exception('Error qlo')
